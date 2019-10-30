328e68d4bd387f3197321b0a44d44929290963b8
arturodl <arturo.04@gmail.com>
2019-10-30 17:13:59 -0600

    fixing some problems on run.sh files and adding new parent pom

--- confserv/src/main/docker/run.sh
+++ confserv/src/main/docker/run.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
+
+echo "********************************************************"
+echo "Waiting for the eureka server to start on port $(getPort $EUREKASERVER_PORT)"
+echo "********************************************************"
+while ! `nc -z eurekaserver  $(getPort $EUREKASERVER_PORT)`; do sleep 3; done
+echo "******* Eureka Server has started"
+
+
 echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
